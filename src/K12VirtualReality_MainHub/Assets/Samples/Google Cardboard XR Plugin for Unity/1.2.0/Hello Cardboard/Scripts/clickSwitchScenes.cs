bc043646268f059b71bce29d4dc843c204f1a3c4
Megan Spiers <Spiers4.13@gmail.com>
2020-10-05 17:25:36 -0400

    Fading Scene Transition

---
+++ src/K12VirtualReality_MainHub/Assets/Samples/Google Cardboard XR Plugin for Unity/1.2.0/Hello Cardboard/Scripts/clickSwitchScenes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class clickSwitchScenes : MonoBehaviour {
+
+	public Animator animator;
+
+    public void buttonChangeScenes(string sceneName){
+    	animator.SetTrigger("Fade_Out");
+    	SceneManager.LoadScene(sceneName);
+    }
+
+
+    
+    
+}
