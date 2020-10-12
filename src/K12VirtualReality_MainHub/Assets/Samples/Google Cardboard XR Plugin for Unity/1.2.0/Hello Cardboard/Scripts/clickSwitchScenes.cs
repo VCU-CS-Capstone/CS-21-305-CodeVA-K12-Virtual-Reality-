4178a681c22cef2efb04cfb1e8b3bfb962822474
Megan Spiers <Spiers4.13@gmail.com>
2020-10-12 14:04:51 -0400

    Fixed Fade Out/In Animations

--- src/K12VirtualReality_MainHub/Assets/Samples/Google Cardboard XR Plugin for Unity/1.2.0/Hello Cardboard/Scripts/clickSwitchScenes.cs
+++ src/K12VirtualReality_MainHub/Assets/Samples/Google Cardboard XR Plugin for Unity/1.2.0/Hello Cardboard/Scripts/clickSwitchScenes.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class clickSwitchScenes : MonoBehaviour {
 
 	public Animator animator;
+	public Image blackFadeImage;
 
     public void buttonChangeScenes(string sceneName){
-    	animator.SetTrigger("Fade_Out");
+    	
+    	StartCoroutine(FadeOutAndIn(sceneName));
+    	
+    }
+
+    IEnumerator FadeOutAndIn(string sceneName) {
+    	animator.SetBool("Fade_Out_And_In_Trigger", true);
+    	yield return new WaitUntil(()=>blackFadeImage.color.a==1);
     	SceneManager.LoadScene(sceneName);
     }
 
