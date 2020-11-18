14faed0f9af2ea98046a4bea1edc32e3e9d248c4
chandlerje@vcu.edu <chandlerje@vcu.edu>
2020-11-18 14:58:09 -0500

    Made vector hidden and equal to Vector3.up

--- src/K12VirtualReality_MainHub/Assets/Revolution.cs
+++ src/K12VirtualReality_MainHub/Assets/Revolution.cs
@@ -7,18 +7,20 @@ public class Revolution : MonoBehaviour
     // Float to hold the number of days per year.
     public float DaysPerYear;
     // Three component vector to hold the revolution of the object.
-    public Vector3 RevolutionVector;
+    private Vector3 _RevolutionVector;
     // The game object to revolve around.
     public GameObject ObjectToRevolveAround;
 
     // Float for constant speed value. 
-    private float speedConstant;
+    private float _SpeedConstant;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Set the revolution vector to Vector3.up.
+        _RevolutionVector = Vector3.up;
         // Set the speed constant to an arbitrary float of 3000.0f.
-        speedConstant = 3000.0f;
+        _SpeedConstant = 3000.0f;
     }
 
     // Update is called once per frame
@@ -26,16 +28,16 @@ public class Revolution : MonoBehaviour
     {
         // Check if any of the axis of the revolution vector is zero.
         // If any of the axis are zero, set the revolution vector to Vector3.up so that the math will still work.
-        if (RevolutionVector.x == 0 && RevolutionVector.y == 0 && RevolutionVector.z == 0) RevolutionVector = Vector3.up;
+        if (_RevolutionVector.x == 0 && _RevolutionVector.y == 0 && _RevolutionVector.z == 0) _RevolutionVector = Vector3.up;
 
         // Calculate the angle in which we want to rotate by dividing deltaTime by the product
         // of daysPerYear and speedConstant.
-        float angle = Time.deltaTime / DaysPerYear * speedConstant;
+        //float angle = ;
 
         // Transform the object using the RotateAround function, passing in the following:
         // - Transform position of the object we want to revolve around.
         // - The new revolution vector.
         // - The angle in which we want to rotate around.
-        transform.RotateAround(ObjectToRevolveAround.transform.position, RevolutionVector, angle);
+        transform.RotateAround(ObjectToRevolveAround.transform.position, _RevolutionVector, Time.deltaTime / DaysPerYear * _SpeedConstant);
     }
 }
