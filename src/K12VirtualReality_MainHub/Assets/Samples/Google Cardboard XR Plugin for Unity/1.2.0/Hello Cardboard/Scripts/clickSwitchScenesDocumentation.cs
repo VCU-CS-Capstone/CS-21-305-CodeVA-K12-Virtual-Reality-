ba646c09b0a6481f506284383b6ce78d9fbaeee8
Megan Spiers <Spiers4.13@gmail.com>
2021-01-15 16:30:01 -0500

    Added Fading scene transition

--- src/K12VirtualReality_MainHub/Assets/Samples/Google Cardboard XR Plugin for Unity/1.2.0/Hello Cardboard/Scripts/clickSwitchScenesDocumentation.cs
+++ src/K12VirtualReality_MainHub/Assets/Samples/Google Cardboard XR Plugin for Unity/1.2.0/Hello Cardboard/Scripts/clickSwitchScenesDocumentation.cs
@@ -6,34 +6,36 @@ using UnityEngine.UI;
 
 public class clickSwitchScenesDocumentation : MonoBehaviour {
 
-	/*public Animator animator;
+	// These are public variables we can edit inside Unity
+	public Animator animator;
 	public Image blackFadeImageFront;
 	public Image blackFadeImageLeft;
 	public Image blackFadeImageRight;
 	public Image blackFadeImageBack;
 	public Image blackFadeImageTop;
 	public Image blackFadeImageBottom;
-	*/
-
-
+	
     public void buttonChangeScenesDocumentation(string sceneName){
     	
-    	//StartCoroutine(FadeOutAndIn(sceneName));
-    	SceneManager.LoadScene(sceneName);
+    	// Start the Coroutine to switch scenes
+    	StartCoroutine(FadeOutAndIn(sceneName));
     	
     }
 
-    /*IEnumerator FadeOutAndIn(string sceneName) {
-    	animator.SetBool("Fade_Out_And_In_Trigger", true);
+    IEnumerator FadeOutAndIn(string sceneName) {
+
+    	// Set the trigger to true to "trigger" the animation
+    	animator.SetBool("Scene_Fade_In_And_Out_Trigger", true);
+
+    	// wait until images fully fade to black to change scenes
     	yield return new WaitUntil(()=>(blackFadeImageFront.color.a==1)&&
     									(blackFadeImageLeft.color.a==1)&&
     									(blackFadeImageRight.color.a==1)&&
     									(blackFadeImageBack.color.a==1)&&
     									(blackFadeImageTop.color.a==1)&&
     									(blackFadeImageBottom.color.a==1));
+
+    	// Switch to the inputted scene
     	SceneManager.LoadScene(sceneName);
-    }*/
-
-
-  	
+    }
 }
