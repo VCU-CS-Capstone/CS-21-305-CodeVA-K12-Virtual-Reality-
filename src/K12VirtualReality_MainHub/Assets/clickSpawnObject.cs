4cfc37ce100fd01f356db162e8a78eec367de36a
Megan Spiers <Spiers4.13@gmail.com>
2021-02-01 10:51:39 -0500

    Initial Commit

---
+++ src/K12VirtualReality_MainHub/Assets/clickSpawnObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickSpawnObject : MonoBehaviour {
+    
+    public Transform spawnPosition1;
+    public Transform spawnPosition2;
+    public Transform spawnPosition3;
+    public GameObject spawnee;
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetButton()) {
+        	Instantiate(spawnee, spawnPosition1.position, spawnPosition1.rotation);
+        	Instantiate(spawnee, spawnPosition2.position, spawnPosition2.rotation);
+        	Instantiate(spawnee, spawnPosition3.position, spawnPosition3.rotation);
+        }
+    }
+}
