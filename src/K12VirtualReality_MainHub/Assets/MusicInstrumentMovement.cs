e3bc002ac1a0eb139235cb2af139c1578dc4b42e
Megan Spiers <Spiers4.13@gmail.com>
2021-02-05 13:11:25 -0500

    Added Instruments

--- src/K12VirtualReality_MainHub/Assets/MusicInstrumentMovement.cs
+++ src/K12VirtualReality_MainHub/Assets/MusicInstrumentMovement.cs
@@ -2,37 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicInstrumentMovement : MonoBehaviour
-{
-    public GameObject ins1;
-    public GameObject ins2;
-    public GameObject ins3;
+public class MusicInstrumentMovement : MonoBehaviour {
 
-    public float amplitude = 0.01f;
+	public GameObject instrument;
+    
+ 
+    public float amplitude = 0.025f;
     public float frequency = 0.5f;
 
-    private GameObject[] instruments;
-
-    private Vector3[] posOffsets; 
-    private Vector3[] tempPositions;
+    private Vector3 posOffset;
+    private Vector3 tempPosition;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        instruments = new GameObject[] { ins1, ins2, ins3 };
-        posOffsets = new Vector3[] { ins1.transform.position, ins2.transform.position, ins3.transform.position };
-        tempPositions = new Vector3[3];
+    void Start() {
+
+        posOffset = instrument.transform.position;
+        
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        for(int i = 0; i < instruments.Length; i++)
-        {
-            tempPositions[i] = posOffsets[i];
-            tempPositions[i].y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+    void Update() {
+        
+    	tempPosition = posOffset;
+    	tempPosition.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+    	instrument.transform.position = tempPosition;
 
-            instruments[i].transform.position = tempPositions[i];
-        }
     }
 }
---
+++ src/K12VirtualReality_MainHub/Assets/objectFaceCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectFaceCamera : MonoBehaviour {
+
+	private Transform cameraToFace;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    	cameraToFace = Camera.main.transform;
+        
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+
+    	transform.LookAt(cameraToFace);
+        
+    }
+}
