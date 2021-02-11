d837fd8e31bf7539061ac9c63fd461959c430d7b
Megan Spiers <Spiers4.13@gmail.com>
2021-02-10 19:03:04 -0500

    Remove Instruments from ArrayList with Each Click

--- src/K12VirtualReality_MainHub/Assets/Music/clickSpawnObject.cs
+++ src/K12VirtualReality_MainHub/Assets/Music/clickSpawnObject.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class clickSpawnObject : MonoBehaviour {
 
-	public GameObject[] spawnees;
-    
+	//public GameObject[] spawnees;
+    public List<GameObject> spawnees;
+
     public Transform spawnPosition1;
     public Transform spawnPosition2;
     public Transform spawnPosition3;
@@ -25,29 +26,67 @@ public class clickSpawnObject : MonoBehaviour {
 
     void spawnRandom() {
 
-    	int flag = 0;
+    	if (spawnees.Count == 3) {
+    		Instantiate(spawnees[0], spawnPosition1.position, spawnPosition1.rotation);
+	   		Instantiate(spawnees[1], spawnPosition2.position, spawnPosition2.rotation);
+	   		Instantiate(spawnees[2], spawnPosition3.position, spawnPosition3.rotation);
+	   		spawnees.RemoveAt(2);
+	   		spawnees.RemoveAt(1);
+	   		spawnees.RemoveAt(0);
 
-    	randomInt1 = Random.Range(0, spawnees.Length);
-    	while (flag == 0) {
-    		randomInt2 = Random.Range(0, spawnees.Length);
-    		if (randomInt2 != randomInt1) {
-    			flag = 1;
-    		}
-    	}
-    	
-    	flag = 0;
-
-    	while (flag == 0) {
-    		randomInt3 = Random.Range(0, spawnees.Length);
-    		if ((randomInt3 != randomInt1) && (randomInt3 != randomInt2)) {
-    			flag = 1;
-    		}
     	}
 
+    	else if (spawnees.Count > 3) {
+	    	int flag = 0;
 
-    	Instantiate(spawnees[randomInt1], spawnPosition1.position, spawnPosition1.rotation);
-    	Instantiate(spawnees[randomInt2], spawnPosition2.position, spawnPosition2.rotation);
-    	Instantiate(spawnees[randomInt3], spawnPosition3.position, spawnPosition3.rotation);
+	   		//randomInt1 = Random.Range(0, spawnees.Length);
 
+	   		randomInt1 = Random.Range(0, (spawnees.Count - 1));
+	   		while (flag == 0) {
+	   			//randomInt2 = Random.Range(0, spawnees.Length);
+    			randomInt2 = Random.Range(0, (spawnees.Count - 1));
+    			if (randomInt2 != randomInt1) {
+	    			flag = 1;
+	   			}
+	   		}
+	    	
+	   		flag = 0;
+
+	   		while (flag == 0) {
+	    		//randomInt3 = Random.Range(0, spawnees.Length);
+	   			randomInt3 = Random.Range(0, (spawnees.Count - 1));
+	   			if ((randomInt3 != randomInt1) && (randomInt3 != randomInt2)) {
+	   				flag = 1;
+	   			}
+    		}
+
+
+	   		Instantiate(spawnees[randomInt1], spawnPosition1.position, spawnPosition1.rotation);
+	   		Instantiate(spawnees[randomInt2], spawnPosition2.position, spawnPosition2.rotation);
+	   		Instantiate(spawnees[randomInt3], spawnPosition3.position, spawnPosition3.rotation);
+
+	   		if (randomInt1 < randomInt2) {
+    			randomInt2 = randomInt2 - 1;
+    		}
+	   		if (randomInt1 < randomInt3) {
+	   			randomInt3 = randomInt3 - 1;
+	   		}
+
+
+	   		if (randomInt2 < randomInt3) {
+	   			randomInt3 = randomInt3 - 1;
+    		}
+
+
+
+	   		spawnees.RemoveAt(randomInt1);
+    		spawnees.RemoveAt(randomInt2);
+	   		spawnees.RemoveAt(randomInt3);
+	    }
+
+	    else {
+	    	return;
+	    }
     }
+
 }
