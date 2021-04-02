dd1f9ec9f14bb27fd3a8fc57cb47ae1e593a23b4
Megan Spiers <Spiers4.13@gmail.com>
2021-04-02 11:49:54 -0400

    All scripts commented up

--- src/K12VirtualReality_MainHub/Assets/Music/MusicInstrumentMovement.cs
+++ src/K12VirtualReality_MainHub/Assets/Music/MusicInstrumentMovement.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class MusicInstrumentMovement : MonoBehaviour {
 
+    // Reference to which instrument will be floating
 	public GameObject instrument;
     
- 
+    // Determine what range the instrument will float within
     public float amplitude = 0.025f;
     public float frequency = 0.5f;
 
+    // Determine positions instrument will float between
     private Vector3 posOffset;
     private Vector3 tempPosition;
 
     // Start is called before the first frame update
     void Start() {
 
+        // Set initial position of instrument
         posOffset = instrument.transform.position;
         
     }
@@ -23,6 +26,7 @@ public class MusicInstrumentMovement : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         
+        // Set temp position to original position, move to new calulated position, return to original position
     	tempPosition = posOffset;
     	tempPosition.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
     	instrument.transform.position = tempPosition;
